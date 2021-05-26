0aba469ab637bcd03b6d62143636d5a1cb3aeeea
ShubhmGupta <>
2021-05-26 14:58:28 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <bits/stdc++.h>
+
+int main() {
+  std::cout << "Hello World!\n";
+
+	double num1 = 10.2;
+	double num2 = 10.2;
+	double num3 = sqrt(num1-num2);
+	std::cout << "Value is = "<<num3;
+}
