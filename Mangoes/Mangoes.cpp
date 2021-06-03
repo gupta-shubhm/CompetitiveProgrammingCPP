c5f6502b374bf404fe5dbbcabe9172d55df9239e
Shubham Gupta <shubham9646@gmail.com>
2021-06-03 12:23:09 +0000

    Mangoes Program

---
+++ Mangoes/Mangoes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int weight;
+cin >> weight;
+int halfNum = weight/2;
+
+if(weight%2==0 && weight!=2){
+cout<<"YES";
+}
+else{cout<<"NO";}
+}
